cedfa452001e744c7c9f82f74d2f5f30d5d08ece
AsuMagic <sdelang@sdelang.fr>
2018-07-11 23:55:49 +0200

    Faster verilator build times

--- verilator_build.sh
+++ verilator_build.sh
@@ -4,11 +4,11 @@ mkdir build &> /dev/null
 cd build
 
 echo "=> Running verilator"
-verilator -Wall -O3 -I../rtl/ -cc ../rtl/cpu.sv --top-module cpu --exe ../../tb/verilator/sim_main.cpp
+verilator -Wall -I../rtl/ -cc ../rtl/cpu.sv --top-module cpu --exe ../../tb/verilator/sim_main.cpp
 
 cd obj_dir
 
 echo "=> Building verilator sources"
-make OPT_FAST="-O3" -f ./Vcpu.mk Vcpu -j4
+make OPT_FAST="-Os" -f ./Vcpu.mk Vcpu -j4
 
 cd ../../
